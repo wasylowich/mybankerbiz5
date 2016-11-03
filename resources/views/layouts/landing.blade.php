71990a04eedb729cdd3e282d3c61926b61c5b396
Brian Wasylowich <wasylowich@gmail.com>
2016-11-04 00:45:01 +0100

    Updates the meta data for the landing page

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -6,24 +6,17 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Adminlte-laravel - {{ trans('adminlte_lang::message.landingdescription') }} ">
-    <meta name="author" content="Sergi Tur Badenas - acacha.org">
+    <meta name="description" content="Mybanker.biz - Let financial institutions bid for you deposit so you can maximize your return. ">
+    <meta name="author" content="Mybanker.biz development team">
 
-    <meta property="og:title" content="Adminlte-laravel" />
+    <meta property="og:title" content="Mybanker.biz" />
     <meta property="og:type" content="website" />
-    <meta property="og:description" content="Adminlte-laravel - {{ trans('adminlte_lang::message.landingdescription') }}" />
-    <meta property="og:url" content="http://demo.adminlte.acacha.org/" />
-    <meta property="og:image" content="http://demo.adminlte.acacha.org/img/AcachaAdminLTE.png" />
-    <meta property="og:image" content="http://demo.adminlte.acacha.org/img/AcachaAdminLTE600x600.png" />
-    <meta property="og:image" content="http://demo.adminlte.acacha.org/img/AcachaAdminLTE600x314.png" />
-    <meta property="og:sitename" content="demo.adminlte.acacha.org" />
-    <meta property="og:url" content="http://demo.adminlte.acacha.org" />
+    <meta property="og:description" content="Mybanker.biz - {{ trans('adminlte_lang::message.landingdescription') }}" />
+    <meta property="og:url" content="http://mybanker.biz/" />
+    <meta property="og:sitename" content="mybanker.biz" />
+    <meta property="og:url" content="http://mybanker.biz" />
 
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:site" content="@acachawiki" />
-    <meta name="twitter:creator" content="@acacha1" />
-
-    <title>{{ trans('adminlte_lang::message.landingdescriptionpratt') }}</title>
+    <title>Mybanker.biz</title>
 
     <!-- Bootstrap core CSS -->
     <link href="{{ asset('/css/bootstrap.css') }}" rel="stylesheet">
@@ -51,7 +44,7 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#"><b>adminlte-laravel</b></a>
+            <a class="navbar-brand" href="#"><b>Mybanker.biz</b></a>
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
